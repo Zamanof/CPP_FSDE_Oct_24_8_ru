9fc36def5bbd268df01bab17072420bc1d7d5f85
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-03-07 20:30:37 +0400

    colors

---
+++ CPP 22. Colors/enums.h
@@ -0,0 +1,32 @@
+#pragma once
+enum ñolors {
+	BLACK,
+	BLUE,
+	GREEN,
+	AQUA,
+	RED,
+	PURPLE,
+	YELLOW,
+	WHITE,
+	GRAY,
+	LIGHT_BLUE,
+	LIGHT_GREEN,
+	LIGHT_AQUA,
+	LIGHT_RED,
+	LIGHT_PURPLE,
+	LIGHT_YELLOW,
+	BRIGHT_WHITE
+};
+
+enum sides {
+	EXIT = 13,
+	UPLETTER = 'w',
+	DOWNLETTER = 's',
+	LEFTLETTER = 'a',
+	RIGHTLETTER = 'd',
+	UPARROW = 72,
+	DOWNARROW = 80,
+	LEFTARROW = 75,
+	RIGHARROW = 77
+
+};
